c9ed9e5b21d9fbad1f524c36dde07ec19d46a424
shin-jewon <79963686+shin-jewon@users.noreply.github.com>
2023-12-19 14:01:53 +0900

    Update m_market.js

--- js/m_market.js
+++ js/m_market.js
@@ -216,7 +216,7 @@ function sendKakaoImage(){
 
 }
 function sendKakaoTmp(imageUrl){
-	alert("sendKakaoTmp 실행");
+	alert("sendKakaoTmp 실행 : "+imageUrl);
 	Kakao.Share.sendDefault({
 			 objectType: 'feed',
 			  content: {
